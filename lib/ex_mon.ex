5cf4b20b104756ed20013566fda4d878705726b8
Breno <breno@email.com>
2024-02-18 17:50:28 -0300

    ultima sec de estudo

--- lib/ex_mon.ex
+++ lib/ex_mon.ex
@@ -1,5 +1,6 @@
 defmodule ExMon do
   alias ExMon.{Game, Player}
+  alias ExMon.Game.{Status, Actions}
   @computer_name "Pc do Mal"
   def create_player(name, move_avg, move_rnd, move_heal) do
     Player.build(name, move_rnd, move_avg, move_heal)
@@ -9,5 +10,15 @@ defmodule ExMon do
     @computer_name
     |> create_player(:punch, :kick, :heal)
     |> Game.start(player)
+
+    Status.print_round_message()
   end
+
+  def make_move(move) do
+    move
+    |> Actions.fetch_move()
+    |> do_move()
+  end
+
+  defp do_move({:error, move}), do: Status.print_wrong_move_message(move)
 end
